50e6c11f8d5615119eacfdf4da80f4f8b463e27b
John Christian P. Oraa <51404378+johnchristianoraa@users.noreply.github.com>
2020-07-27 18:17:44 +0800

    layout header and banner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-header">
+          <div className="title">
+            <h1>OAAM</h1>
+            <nav>
+              <ul>
+                <li><a>Movies</a></li>
+                <li><a>TV Shows</a></li>
+                <li><a>People</a></li>
+                <li><a>More</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div className="search">
+            <a> Search </a>
+          </div>
       </header>
+    
+      <section className="showcase">
+        <div className="container">
+          <h1>Welcome.</h1>
+          <p>Millions of movies, TV shows and people to discover. Explore now.</p>
+          <input type="text" placeholder="Search for Movie, TV Shows, People ..." />
+        </div>
+      </section>
+
+      <section className="">
+
+      </section>
     </div>
   );
 }
